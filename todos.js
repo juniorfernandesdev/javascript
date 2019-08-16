1e78c9d3980ebe82e26badf6db88541d38d1a944
Junior Fernandes <juniorbass8@gmail.com>
2019-08-16 09:20:02 -0300

    Finalização do TODO

--- todos.js
+++ todos.js
@@ -1,3 +1,53 @@
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar javascript',
+    'acessar comunidade rocketseat'
+];
+
+function renderTodo () {
+    listElement.innerHTML = '';
+    //for especifico para array
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodo(); 
+
+function addTodo () {
+    var todoText = inputElement.value;
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodo();
+
+}
+//pos = recebe posição
+function deleteTodo (pos) {
+    //splice remove quantidade de items do array
+    todos.splice(pos, 1);
+    renderTodo();
+}
+
+buttonElement.onclick = addTodo;
